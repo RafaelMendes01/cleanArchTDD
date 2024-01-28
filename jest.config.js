c66a6f4bc8c8b9885489dba6d9926ccee6dca104
Rafael Mendes <rafael.mendes@tallos.com.br>
2024-01-28 08:48:25 -0300

    chore: ignoring protocol files jest

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,11 @@
 module.exports = {
   roots: ['<rootDir>/src'],
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/presentation/protocols/index.ts',
+    '!<rootDir>/src/presentation/controllers/signup/signup-protocols.ts',
+    '!<rootDir>/src/data/usecases/add-account/db-add-account-protocols.ts'
+  ],
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
   transform: {
