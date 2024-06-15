640ab67074dcab017b26d91b48a98a2172110df6
Rafael Mendes <rafael.mendes@tallos.com.br>
2024-06-15 14:47:09 -0300

    feat: initializing node server

---
+++ src/main/server.ts
@@ -0,0 +1,5 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const express = require('express')
+
+const app = express()
+app.listen(5050, () => console.log('Server running at https://localhost:5050'))
